fd9f3f55a861931ff9cfa372487561bd5fd255c7
Bouvard <xavier.bouvard@supinfo.com>
2012-04-09 01:18:05 +0300

    Update include/lunarNXT/LineFollower.h

--- include/lunarNXT/LineFollower.h
+++ include/lunarNXT/LineFollower.h
@@ -11,16 +11,20 @@
 
 class LineFollower : public Mode {
 private:
+	// gestion des déplacements
 	MoveMgr* mm;
 
+	// Attributs
 	nxt_msgs::Color colorMsg;
 	float color[3];
 	bool online;
 	std::string direction;
 
+	// traitement du mode
 	void treat();	
 	
 public:
+	// constructeurs et destructeur
 	LineFollower();
 	LineFollower(MoveMgr* mm);
 	void updateColor(nxt_msgs::Color msg);
