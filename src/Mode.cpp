c7bd00b9f7edae060979380b533c0b7f2468e526
Bouvard <xavier.bouvard@supinfo.com>
2012-04-09 01:38:05 +0300

    Update src/Mode.cpp

--- src/Mode.cpp
+++ src/Mode.cpp
@@ -1,23 +1,31 @@
 #include "lunarNXT/Mode.h"
 
+// Constructeur
 Mode::Mode(MoveMgr* mm) : Receptor(mm->getNameLeftMotor(), mm->getNameRightMotor()) { 
 	this->mm = mm; 
 	this->launched = false;
 	this->initialized = false;
 }
 
+// Destructeur
 Mode::~Mode() { ; }
 
+// Getters
 bool Mode::isLaunched()    { return this->launched; }
 bool Mode::isInitialized() { return this->initialized; }
 
+// Setter
 void Mode::setInitialized(bool init) { this->initialized = init; }
 
+// Methode de lancement du mode
 void Mode::launch() {
         this->launched = true;
         this->initialized = false;
 };
 
+
+// Methode d'arret du mode
+// Donne l'ordre MoveMgr de stopper les moteurs
 void Mode::unlaunch() {
         this->launched = false;
         this->initialized = false;
